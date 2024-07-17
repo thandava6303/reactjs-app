093e7a299731e84afee1ce2f81da20cc920153ad
Hari Kammana <hari.kammana@gmail.com>
2024-07-17 14:57:51 +0530

    reactjs setup

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HelloWorld = () => {
+  return (
+    <div className="hello-container">
+      <h1 className="hello-text">ReactJS CI/CD</h1>
+    </div>
+  );
+};
+
+export default HelloWorld;
