1e5676c538bd56e73fedcd70bae255e6bc9c1543
javahometech <hari.kammana@gmail.com>
2024-07-17 18:18:40 +0530

    Update HelloWorld.jsx

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HelloWorld = () => {
   return (
     <div className="hello-container">
-      <h1 className="hello-text">ReactJS CI/CD Demo</h1>
+      <h1 className="hello-text">ReactJS CI/CD Java Home Cloud</h1>
     </div>
   );
 };
