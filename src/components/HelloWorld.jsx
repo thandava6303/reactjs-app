d7724c7cfc148aecf4150b4fd93f98a53ba12f25
thandava6303 <80169246+thandava6303@users.noreply.github.com>
2025-09-25 01:02:38 +0530

    Update HelloWorld.jsx

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,11 +3,12 @@ import React from 'react';
 const HelloWorld = () => {
   return (
     <div className="hello-container">
-      <h1 className="hello-text">welcome to priacc innovations pvt ltd....</h1>
+      <h1 className="hello-text">welcome to priacc innovations pvt ltd..</h1>
     </div>
   );
 };
 
 export default HelloWorld;
+
 
 
